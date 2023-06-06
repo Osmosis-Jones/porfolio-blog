c75a62bc08bbb77ad4a4ae68c48061b21283cb78
Osmosis-Jones <56321184+Osmosis-Jones@users.noreply.github.com>
2023-06-06 21:42:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Navbar from './nav';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="content">
+        <h1>App Component</h1>
+        <Home/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
